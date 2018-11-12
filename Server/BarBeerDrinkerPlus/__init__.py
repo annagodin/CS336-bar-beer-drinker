5280a8cce4683402f9109455e890c96153feae84
Anna Godin <anna.godin@alice-app.com>
2018-11-11 23:51:36 -0500

    queries and shit

--- Server/BarBeerDrinkerPlus/__init__.py
+++ Server/BarBeerDrinkerPlus/__init__.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 def get_bars():
     return jsonify(database.get_bars())
 
- 
 @app.route("/api/bar/<name>", methods=["GET"])
 def find_bar(name):
     try:
@@ -27,13 +26,11 @@ def find_bar(name):
     except Exception as e:
         return make_response(str(e), 500)
 
-
 @app.route("/api/beers_cheaper_than", methods=["POST"])
 def find_beers_cheaper_than():
     body = json.loads(request.data)
     max_price = body['maxPrice']
     return jsonify(database.filter_beers(max_price))
-
 
 @app.route('/api/menu/<name>', methods=['GET'])
 def get_menu(name):
@@ -49,14 +46,12 @@ def get_menu(name):
     except Exception as e:
         return make_response(str(e), 500)
 
-
 @app.route("/api/bar-cities", methods=["GET"])
 def get_bar_cities():
     try:
         return jsonify(database.get_bar_cities())
     except Exception as e:
         return make_response(str(e), 500)
-
 
 @app.route("/api/beer", methods=["GET"])
 def get_beers():
@@ -65,14 +60,12 @@ def get_beers():
     except Exception as e:
         return make_response(str(e), 500)
 
-
 @app.route("/api/beer-manufacturer", methods=["GET"])
 def get_beer_manufacturers():
     try:
         return jsonify(database.get_beer_manufacturers(None))
     except Exception as e:
         return make_response(str(e), 500)
-
 
 @app.route("/api/beer-manufacturer/<beer>", methods=["GET"])
 def get_manufacturers_making(beer):
@@ -81,8 +74,8 @@ def get_manufacturers_making(beer):
     except Exception as e:
         return make_response(str(e), 500)
 
-
  #WTF IS UP WITH THIS ONE??????
+
 @app.route("/api/likes", methods=["GET"])
 def get_likes():
     try:
@@ -93,14 +86,12 @@ def get_likes():
     except Exception as e:
         return make_response(str(e), 500)
 
-
 @app.route("/api/customer", methods=["GET"])
 def get_customers():
     try:
         return jsonify(database.get_customers())
     except Exception as e:
         return make_response(str(e), 500)
-
 
 @app.route("/api/customer/<name>", methods=["GET"])
 def get_customer(name):
@@ -113,7 +104,6 @@ def get_customer(name):
     except Exception as e:
         return make_response(str(e), 500)
 
-
 @app.route('/api/bars-selling/<beer>', methods=['GET'])
 def find_bars_selling(beer):
     try:
@@ -124,7 +114,6 @@ def find_bars_selling(beer):
         return make_response(str(e), 400)
     except Exception as e:
         return make_response(str(e), 500)
-
 
 @app.route('/api/frequents-data', methods=['GET'])
 def get_bar_frequent_counts():
@@ -139,5 +128,52 @@ def get_customer_transactions(name):
         if name is None:
             raise ValueError("Cutsomer is not specified.")
         return jsonify(database.get_customer_transactions(name))
+    except Exception as e:
+        return make_response(str(e), 500)
+
+@app.route('/api/top-beers/<customer>', methods=['GET'])
+def get_top_beers_bought(customer):
+    try:
+        if customer is None:   
+            raise ValueError("Customer is not specified.")
+        return jsonify(database.get_top_beers_bought(customer))
+    except Exception as e:
+        return make_response(str(e), 500)
+
+# FOR THE GRAPH FOR 
+# bar graph of his/her spending on different dates.
+@app.route('/api/total-spending-day/<customer>', methods=['GET'])
+def get_total_spending_per_day(customer):
+    try:
+        if customer is None:   
+            raise ValueError("Customer is not specified.")
+        return jsonify(database.get_total_spending_per_day(customer))
+    except Exception as e:
+        return make_response(str(e), 500)
+
+@app.route('/api/total-spending-bar/<customer>', methods=['GET'])
+def get_total_spending_per_bar(customer):
+    try:
+        if customer is None:   
+            raise ValueError("Customer is not specified.")
+        return jsonify(database.get_total_spending_per_bar(customer))
+    except Exception as e:
+        return make_response(str(e), 500)
+
+@app.route('/api/top-spenders/<bar>', methods=['GET'])
+def get_top_spenders_per_bar(bar):
+    try:
+        if bar is None:
+            raise ValueError("Bar is not specified.")
+        return jsonify(database.get_top_spenders_per_bar(bar))
+    except Exception as e:
+        return make_response(str(e), 500)
+
+@app.route('/api/top-beers-bar/<bar>', methods=['GET'])
+def get_top_beers_per_bar(bar):
+    try:
+        if bar is None:
+            raise ValueError("Bar is not specified")
+        return jsonify(database.get_top_beers_per_bar(bar))
     except Exception as e:
         return make_response(str(e), 500)
--- Server/BarBeerDrinkerPlus/database.py
+++ Server/BarBeerDrinkerPlus/database.py
@@ -10,7 +10,6 @@ def get_bars():
         rs = con.execute("SELECT Bar, City, Phone, License FROM BarBeerDrinkerPlus.Bars;")
         return [dict(row) for row in rs]
 
- 
 def find_bar(name):
     with engine.connect() as con: 
         query = sql.text(
@@ -35,7 +34,6 @@ def filter_beers(max_price):
             r['Price'] = float(r['Price'])
         return results
 
-
 def get_bar_menu(bar_name):
     with engine.connect() as con:
         query = sql.text(
@@ -54,7 +52,6 @@ def get_bar_menu(bar_name):
             results[i]['Price'] = float(results[i]['Price'])
         return results
 
-
 def get_bars_selling(beer):
     with engine.connect() as con:
         query = sql.text(
@@ -71,7 +68,6 @@ def get_bars_selling(beer):
             results[i]['Price'] = float(results[i]['Price'])
         return results
 
-
 def get_bar_frequent_counts():
     with engine.connect() as con:
         query = sql.text('SELECT Bar, count(*) as frequentCount \
@@ -83,12 +79,10 @@ def get_bar_frequent_counts():
         results = [dict(row) for row in rs]
         return results
 
-
 def get_bar_cities():
     with engine.connect() as con:
         rs = con.execute('SELECT DISTINCT City FROM Bars;')
         return [row['City'] for row in rs]
-
 
 def get_beers():
     """Gets a list of beer names from the beers table."""
@@ -96,7 +90,6 @@ def get_beers():
     with engine.connect() as con:
         rs = con.execute('SELECT Name, Manufacturer FROM Item where Type = "beer";')
         return [dict(row) for row in rs]
-
 
 def get_beer_manufacturers(beer):
     with engine.connect() as con:
@@ -111,21 +104,18 @@ def get_beer_manufacturers(beer):
             return None
         return result['Manufacturer']
 
-
 def get_customers():
     with engine.connect() as con:
         rs = con.execute('SELECT Name, City, Phone FROM Customers;')
         return [dict(row) for row in rs]
 
-
 def get_likes(customer_name):
     """Gets a list of beers liked by the drinker provided."""
 
     with engine.connect() as con:
-        query = sql.text('SELECT Name FROM Likes WHERE Name = :name;')
+        query = sql.text('SELECT Beer FROM Likes WHERE Customer = :name;')
         rs = con.execute(query, name=customer_name)
-        return [row['beer'] for row in rs]
-
+        return [row['Beer'] for row in rs]
 
 def get_customer_info(customer_name):
     with engine.connect() as con:
@@ -135,7 +125,6 @@ def get_customer_info(customer_name):
         if result is None:
             return None
         return dict(result)
-
 
 def get_customer_transactions(name):
     with engine.connect() as con:
@@ -147,3 +136,79 @@ def get_customer_transactions(name):
             ')
         rs = con.execute(query, name=name)
         return [dict(row) for row in rs]
+
+def get_top_beers_bought(customer_name):
+    with engine.connect() as con:
+        query=sql.text('SELECT t.Customer, i.Name, count(*) as numBought \
+            FROM Transactions t, ItemsByID i \
+            WHERE t.ID = i.ID \
+            AND Type = "Beer" \
+            AND t.Customer = :name \
+            GROUP BY i.Name \
+            ORDER BY numBought desc \
+            LIMIT 10 \
+        ')
+        rs = con.execute(query, name=customer_name)
+        results =  [dict(row) for row in rs]
+        for i, _ in enumerate(results):
+            results[i]['numBought'] = float(results[i]['numBought'])
+        return results
+
+def get_total_spending_per_day(customer_name):
+    with engine.connect() as con:
+        query=sql.text('SELECT t.Customer, t.Date, sum(TotalCost) as TotalSpent\
+            FROM Transactions t\
+            Where t.Customer = :name\
+            Group by t.Customer, t.Date\
+            order by STR_TO_DATE(Date,\'%m/%d/%y\')\
+        ')
+        rs = con.execute(query, name=customer_name)
+        results =  [dict(row) for row in rs]
+        for i, _ in enumerate(results):
+            results[i]['TotalSpent'] = float(results[i]['TotalSpent'])
+        return results
+        
+def get_total_spending_per_bar(customer_name):
+    with engine.connect() as con:
+        query=sql.text(' SELECT t.Customer, t.Bar, ROUND(sum(TotalCost),2) as TotalSpent \
+            FROM Transactions t \
+            Where t.Customer = :name \
+            Group by t.Customer, t.Bar \
+            Order by TotalSpent desc \
+        ')
+        rs = con.execute(query, name=customer_name)
+        results =  [dict(row) for row in rs]
+        for i, _ in enumerate(results):
+            results[i]['TotalSpent'] = float(results[i]['TotalSpent'])
+        return results
+
+def get_top_spenders_per_bar(bar_name):
+    with engine.connect() as con:
+        query=sql.text('Select t.Customer, ROUND(sum(TotalCost),2) as TotalSpent \
+            From Transactions t \
+            Where t.Bar = :bar \
+            Group by t.Customer \
+            order by TotalSpent desc \
+        ')
+        rs = con.execute(query, bar = bar_name)
+        results =  [dict(row) for row in rs]
+        for i, _ in enumerate(results):
+            results[i]['TotalSpent'] = float(results[i]['TotalSpent'])
+        return results
+
+def get_top_beers_per_bar(bar_name):
+    with engine.connect() as con:
+        query=sql.text('SELECT i.Name, count(*) as numBought \
+            FROM Transactions t, ItemsByID i \
+            WHERE t.ID = i.ID \
+            AND Type = "Beer" \
+            AND t.Bar = :bar \
+            GROUP BY i.Name \
+            ORDER BY numBought desc \
+            LIMIT 10; \
+        ')
+        rs = con.execute(query, bar = bar_name)
+        results =  [dict(row) for row in rs]
+        # for i, _ in enumerate(results):
+        #     results[i]['numBought'] = float(results[i]['numBought'])
+        return results
